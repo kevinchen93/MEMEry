f02d2640eb2b08c2f1c72dcaf40bd680cebcaaef
Kevin Chen <xkevinchen@gmail.com>
2018-09-27 17:44:25 -0400

    Apply functionality to buttons

---
+++ js/card.js
@@ -0,0 +1,5 @@
+class Card {
+  constructor(imageURL) {
+    this.imageURL = imageURL;
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 const cards = document.querySelectorAll('.card-content');
 
-Array.from(cards).forEach( card => {  
+Array.from(cards).forEach( card => {
   card.addEventListener('click', () => {
     card.classList.toggle('is-flipped');
   });
-})
+});
+
+const startButton = document.getElementsByClassName('start-button')[0];
+const resetButton = document.getElementsByClassName('reset-button')[0];
+
+startButton.addEventListener('click', () => {
+  Array.from(cards).forEach( card => {
+    card.classList.remove('is-flipped');
+  });
+});
+
+resetButton.addEventListener('click', () => {
+  Array.from(cards).forEach( card => {
+    card.classList.remove('is-flipped');
+  });
+});
