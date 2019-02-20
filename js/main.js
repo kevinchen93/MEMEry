0881e986c2f2e0bcbdb8d6ddbd27d7822644d622
Kevin Chen <xkevinchen@gmail.com>
2019-02-20 14:26:09 -0500

    Minor styling adjustments

--- js/main.js
+++ js/main.js
@@ -230,7 +230,7 @@ $('.landing-modal-button').click(function() {
   $('.landing-modal').hide();
   $('.main-content').show();
   audio.src = 'assets/sounds/Nujabes - Luv(sic) Pt. 3 (ft. Shing02).mp3';
-  audio.volume = 0.7;
+  audio.volume = 0.3;
   audio.play();
   resetTime();
 });
